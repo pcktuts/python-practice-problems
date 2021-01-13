4485f9caf30f77dd6aa94c87a7afbb480e215177
Learn Programming In Malayalam <63596426+pcktuts@users.noreply.github.com>
2021-01-13 16:12:42 +0530

    Create 01-list.py

---
+++ 01-list.py
@@ -0,0 +1,12 @@
+"""
+Write a program which checks for all the items in a list, if the 
+value in a list is less than 25 replace the item with
+difference between 25 and that element. if there is no such item print
+the original list.
+
+Example:
+a = [1,2, 45,43, 67,22,33]
+
+will output
+[24, 23, 45, 43, 67, 3, 33]
+"""
