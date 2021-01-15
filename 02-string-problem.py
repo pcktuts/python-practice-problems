66fbc5f2bb842add8aca968be7b645b7dc57e38c
Learn Programming In Malayalam <63596426+pcktuts@users.noreply.github.com>
2021-01-15 12:12:14 +0530

    Create 02-string-problem.py

---
+++ 02-string-problem.py
@@ -0,0 +1,7 @@
+"""
+1. Given a string like below must give output like below.
+accept input from the console and use functions.
+examples
+1. ghhbpcckeee = g2hbp2ck3e(required output)
+2. mhkkkkrtrrr = mh4krt3r(required output)
+"""
